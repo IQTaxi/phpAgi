fd623da112c2af2bc3ce8f723c51e08dfa6e8388
dDimitriadis <dimitris.dimtriadis@gmail.com>
2020-04-06 08:17:37 +0300

    asterisk v0.9: check if number that sends to api contains '+30' or '0'

--- var_lib_asterisk_bin/redirect_call_on_phone.php
+++ var_lib_asterisk_bin/redirect_call_on_phone.php
@@ -3,21 +3,33 @@
   set_time_limit(30);
   require('phpagi.php');
   error_reporting(E_ALL);  
-
+  
   $mPhone= trim($argv[1]);
   $agi = new AGI();
   $agi->answer();
 
-  $returnedPhoneToCall = file_get_contents("http://192.168.24.24/Webapi/api/Asterisk/GetRedirectPhone/".$mPhone);
+  $modPhone = $mPhone;
+#  $modPhone  = "+302310208100"; 
+
+  if (strpos($modPhone, "+30") !== false) {
+      $modPhone = str_replace("+30","",$modPhone);
+#      $agi->exec('NoOp', '@@@'.$modPhone);
+  }
+  if (strpos($modPhone, "+") !== false) {
+      $modPhone = str_replace("+","",$modPhone);
+#      $agi->exec('NoOp', '!!!'.$modPhone);
+  }
+
+  $returnedPhoneToCall = file_get_contents("http://192.168.24.24/Webapi/api/Asterisk/GetRedirectPhone/".$modPhone);
 # $returnedPhoneToCall = '6978527553@berofix_trunk';
 
-  $mTempVar = "http://192.168.24.24/Webapi/api/Asterisk/GetRedirectPhone/".$mPhone;
   $agi->exec('NoOp', '@@'.$returnedPhoneToCall.'@@');
-  $agi->exec('NoOp', $mPhone);
-  $agi->exec('NoOp', $mTempVar);
+  $agi->exec('NoOp', $modPhone);
 
   $agi->exec('dial', 'SIP/'.${returnedPhoneToCall});
 #  $agi->exec('dial', 'SIP/26');
+#	$agi->exec('dial', 'SIP/2310208100@Beronet');
+
 
   $agi->hangup();
 ?>
