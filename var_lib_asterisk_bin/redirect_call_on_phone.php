8e74c7e78a3e542daff4115119304124c3200ec1
Christos Iakovidis <ciakov@gmail.com>
2026-03-13 15:48:47 +0200

    Τransfer to driver fix

--- var_lib_asterisk_bin/redirect_call_on_phone.php
+++ var_lib_asterisk_bin/redirect_call_on_phone.php
@@ -1,45 +1,92 @@
 #!/usr/bin/php -q
 <?php
-  set_time_limit(30);
-  require('phpagi.php');
-  error_reporting(E_ALL);  
+set_time_limit(30);
+error_reporting(E_ALL);
 
-  $mPhone= trim($argv[1]);
-  $agi = new AGI();
-  $agi->answer();
+$logFile = '/var/log/asterisk/redirect_call_debug.log';
+file_put_contents($logFile, date('Y-m-d H:i:s') . " === Script started ===\n", FILE_APPEND);
+file_put_contents($logFile, "argv: " . print_r($argv, true) . "\n", FILE_APPEND);
 
-  $modPhone = $mPhone;
-#  $modPhone  = "+302310208100"; 
+ob_start();
+error_reporting(0);
+require('/var/lib/asterisk/agi-bin/phpagi.php');
+file_put_contents($logFile, "phpagi.php loaded OK\n", FILE_APPEND);
 
-  if (strpos($modPhone, "+30") !== false) {
-      $modPhone = str_replace("+30","",$modPhone);
-#      $agi->exec('NoOp', '@@@'.$modPhone);
-  }
-  if (strpos($modPhone, "+") !== false) {
-      $modPhone = str_replace("+","",$modPhone);
-#      $agi->exec('NoOp', '!!!'.$modPhone);
-  }
+$agi = new AGI();
+$ob_output = ob_get_clean();
+error_reporting(E_ALL);
+file_put_contents($logFile, "AGI object created\n", FILE_APPEND);
+if ($ob_output) {
+  file_put_contents($logFile, "Captured output during AGI init: [$ob_output]\n", FILE_APPEND);
+}
+file_put_contents($logFile, "AGI request keys: " . implode(', ', array_keys($agi->request)) . "\n", FILE_APPEND);
+file_put_contents($logFile, "AGI request: " . print_r($agi->request, true) . "\n", FILE_APPEND);
 
-  $returnedPhoneToCall = file_get_contents("http://192.168.24.24/Webapi/api/Asterisk/GetRedirectPhone/".$modPhone);
+$mPhone = isset($agi->request['agi_arg_1']) ? trim($agi->request['agi_arg_1']) : '';
+file_put_contents($logFile, "mPhone from agi_arg_1: [$mPhone]\n", FILE_APPEND);
+
+if ($mPhone === '') {
+  file_put_contents($logFile, "ERROR: No phone argument received\n", FILE_APPEND);
+  $agi->hangup();
+  exit(0);
+}
+
+$modPhone = $mPhone;
+#  $modPhone  = "+302310208100";
+
+if (strpos($modPhone, "+30") !== false) {
+  $modPhone = str_replace("+30", "", $modPhone);
+  #      $agi->exec('NoOp', '@@@'.$modPhone);
+}
+if (strpos($modPhone, "+") !== false) {
+  $modPhone = str_replace("+", "", $modPhone);
+  #      $agi->exec('NoOp', '!!!'.$modPhone);
+}
+
+file_put_contents($logFile, "modPhone after cleanup: [$modPhone]\n", FILE_APPEND);
+$agi->exec('NoOp', 'Calling API for phone: ' . $modPhone);
+
+$apiUrl = "http://www.iqtaxi.cn/IQTaxiAPIV3/api/Asterisk/GetRedirectPhone/" . $modPhone . "/true/70";
+file_put_contents($logFile, "Calling API: $apiUrl\n", FILE_APPEND);
+
+$returnedPhoneToCall = @file_get_contents($apiUrl);
+file_put_contents($logFile, "API response: [$returnedPhoneToCall]\n", FILE_APPEND);
 # $returnedPhoneToCall = '6978527553@berofix_trunk';
 
-  $agi->exec('NoOp', '@@'.$returnedPhoneToCall.'@@');
-  $agi->exec('NoOp', $modPhone);
-  
-  do {
-#    $agi->exec('dial', 'SIP/'.${returnedPhoneToCall});
-#    $agi->exec('dial', 'SIP/26');
-    $agi->exec('dial', 'SIP/2310314194@berofix_trunk');
-#    $agi->exec('dial', 'SIP/6978527553@berofix_trunk');
-    $dialstatus = $agi->get_variable('DIALSTATUS');
-
-    if($dialstatus[data] != 'ANSWER'){
-      $agi->set_music(true,"myholdclass"); //To start playing music while waiting
-      sleep(3); //Time before each call
-      $agi->exec('NoOp', 'Retry to call because it is : '.$dialstatus[data]); 
-    } 
-    $agi->exec('NoOp', $dialstatus);
-  }while ( $dialstatus[data] != 'ANSWER' );
-  
+if ($returnedPhoneToCall === false || $returnedPhoneToCall === '') {
+  file_put_contents($logFile, "ERROR: API failed or returned empty\n", FILE_APPEND);
+  $agi->exec('NoOp', 'ERROR: API returned no phone number for ' . $modPhone);
   $agi->hangup();
+  exit(0);
+}
+
+$decoded = json_decode($returnedPhoneToCall);
+if ($decoded !== null) {
+  $returnedPhoneToCall = $decoded;
+}
+$returnedPhoneToCall = trim($returnedPhoneToCall);
+# $returnedPhoneToCall = str_replace('@vodafone', '@vodafone_sip', $returnedPhoneToCall);
+$agi->exec('NoOp', '@@' . $returnedPhoneToCall . '@@');
+$agi->exec('NoOp', $modPhone);
+
+do {
+  # ,30,r -> 
+  # 30 :dial timeout 
+  # r  :hear ringtone
+  #    m  :hear music on hold
+  $agi->exec('dial', 'SIP/' . $returnedPhoneToCall . ',30,r');
+  #    $agi->exec('dial', 'SIP/26');
+  #  $agi->exec('dial', 'SIP/2310314194@berofix_trunk');
+  #    $agi->exec('dial', 'SIP/6978527553@berofix_trunk');
+  $dialstatus = $agi->get_variable('DIALSTATUS');
+
+  if ($dialstatus['data'] != 'ANSWER') {
+    $agi->set_music(true, "myholdclass"); //To start playing music while waiting
+    sleep(3); //Time before each call
+    $agi->exec('NoOp', 'Retry to call because it is : ' . $dialstatus['data']);
+  }
+  $agi->exec('NoOp', $dialstatus);
+} while ($dialstatus['data'] != 'ANSWER');
+
+$agi->hangup();
 ?>
